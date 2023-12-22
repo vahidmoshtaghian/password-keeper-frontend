553fa5053addcb74252f9b91ba3b4bb8ac45bd37
Vahid Moshtaghian <vahid.moshtaghian@hotmail.com>
2023-12-22 19:42:05 +0330

    create menu

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-
+    provideAnimations(),
     BrowserModule,
     BrowserAnimationsModule
   ]
--- src/app/pages/base/layout/layout.component.ts
+++ src/app/pages/base/layout/layout.component.ts
@@ -1,15 +1,44 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { SidebarModule } from 'primeng/sidebar';
+import { MenuModule } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [
     CommonModule,
-    RouterOutlet
+    RouterOutlet,
+    ButtonModule,
+    SidebarModule,
+    MenuModule
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
-export class LayoutComponent { }
+export class LayoutComponent implements OnInit {
+  items: MenuItem[] | undefined;
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: "Account Info",
+        icon: "pi pi-user-edit"
+      },
+      {
+        label: "Friends List",
+        icon: "pi pi-users"
+      },
+      {
+        label: '<span style="color: var(--red-600)">Log Out</span>',
+        escape: false,
+        icon: "pi pi-sign-out",
+        iconClass: "red"
+      },
+    ]
+  }
+
+}
